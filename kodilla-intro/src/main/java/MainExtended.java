9255a9eca3cf799fc2e65f677aa440cc19718613
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-01-13 13:14:43 +0100

    Variable Task

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Basia";
+        int myAge = 40;
+        char firstLetterOfName = 'B';
+        double heigth = 1.6;
+        boolean isMother = true;
+        int numberKids = 2;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(firstLetterOfName);
+        System.out.println(heigth);
+        System.out.println(isMother);
+        System.out.println(numberKids);
+
+    }
+}
