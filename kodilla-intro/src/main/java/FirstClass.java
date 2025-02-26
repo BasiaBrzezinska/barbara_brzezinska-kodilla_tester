645016ade15e18144ac490e4e8cc4735742d17a5
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-02-26 15:27:25 +0100

    Leap Year Task

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,12 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello from FirstClass!");
+        int liczba = 1;
+        if (liczba > 2) {
+            System.out.println("Niesamowite! jeden jest wiekszy niz dwa!!!");
+        } else {
+            System.out.println("powinien sie wyświetlic ten!");
+        }
     }
 }
 
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,18 @@
+public class LeapYear {
+    public static void main (String [] args) {
+
+        int year = 2004;
+
+        if ( year % 4 == 0 && year % 100 != 0 ) {
+            System.out.println("Rok jest przystępny");
+        }
+        else if ( year % 100 ==0 && year % 400 == 0 ) {
+            System.out.println("też jest rok przystępny");
+        }
+        else {
+            System.out.println("Rok jest nie przystępny");
+        }
+
+
+    }
+}
