e72a7a213a699c997ed2790ea89265586a288266
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-07 17:50:32 +0100

    zmiana roku

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,7 +1,7 @@
 public class LeapYear {
     public static void main (String [] args) {
 
-        int year = 2004;
+        int year = 1986;
 
         if ( year % 4 == 0 && year % 100 != 0 ) {
             System.out.println("Rok jest przystępny");
---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,12 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animals = new String[5];
+
+        animals[0] = "dog";
+        animals[1] = "cat";
+        animals[2] = "rabit";
+        animals[3] = "hamster";
+        animals[4] = "turtle";
+    }
+    System.out.println.(animals[0]);
+}
