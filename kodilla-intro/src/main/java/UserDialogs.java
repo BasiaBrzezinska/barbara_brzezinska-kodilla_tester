5f674d16dab9d06ac7818953f7c13bc748da88a0
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-18 22:17:17 +0100

    Task_2.8

--- kodilla-intro/src/main/java/UserDialogs.java
+++ kodilla-intro/src/main/java/UserDialogs.java
@@ -11,6 +11,27 @@ public class UserDialogs {
             }
             System.out.println("Name is too short, try again");
         }
+    }
 
+
+    public static String getColourLetter(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Select letter: R - red, B - blue, G - green, W - white, Y - yellow");
+            String color = scanner.nextLine().trim().toUpperCase();
+            switch (color){
+                case "R" : return "red";
+                case "B" : return "blue";
+                case "G" : return "green";
+                case "W" : return "white";
+                case "Y" : return "yellow";
+
+                default: System.out.println("We don't have that color. Select another one");
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        System.out.println("Your color is:" + getColourLetter());
     }
 }
