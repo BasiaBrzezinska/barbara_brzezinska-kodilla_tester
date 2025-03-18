028bc40cfd7cf10867f65ad8b0c3517201c872d1
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-18 21:54:45 +0100

    Task_2.5

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class RandomNumbers {
+
+    public int getCountOfRandomNumbers() {
+        int max = 5000;
+        Random random = new Random();
+        int sum = 0;
+        int result = 0;
+        int maxNumber = Integer.MIN_VALUE;
+        int minNumber = Integer.MAX_VALUE;
+
+        while (sum < max) {
+            int randomNumber = random.nextInt(31);
+            sum = sum + randomNumber;
+            result++;
+
+            if (randomNumber > maxNumber) {
+                maxNumber = randomNumber;
+            }
+
+            if (randomNumber < minNumber) {
+                minNumber = randomNumber;
+            }
+        }
+
+
+        System.out.println("Największ wylosowana liczba to: " + maxNumber);
+        System.out.println("Najmniejsza wylosowana liczna to: " + minNumber);
+        System.out.println("Ilość losowań: " + result);
+        return result;
+
+    }
+
+    public static void main (String[] args){
+        RandomNumbers randomNumbers = new RandomNumbers();
+        System.out.println(randomNumbers.getCountOfRandomNumbers());
+
+    }
 }
+
--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
-
 public class User {
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+        User[] users = new User[5];
+
+        users[0] = new User("Nela", 18);
+        users[1] = new User("Leon", 15);
+        users[2] = new User("Daria", 20);
+        users[3] = new User("Marcin", 38);
+        users[4] = new User("Karolina", 35);
+
+
+        int result = 0;
+
+        for (int i = 0; i < users.length; i++) {
+            result = result + users[i].age;
+            // result = (result + users[i].age)/ users.length; Dlaczego ten sposób jest zły???
+        }
+        System.out.println("Total users age is " +result+ "years old");
+
+        int averageAge = result / users.length;
+        System.out.println("Average users age is :" + averageAge +" years" );
+
+        // public static int averageAge(){
+        //    int average = result/ users.length;
+        //    return average;  Dlaczego nie mogłam utworzyć takiej metody?
+
+        //  for (int i=0; i<users.length;); i++) {
+        //if (users.age<averageAge){
+        //    System.out.println(users.names);
+        //} To też nie zadziałało...
+        //dlaczego tutaj nie mogę odwołac się do user.age??
+
+        for (int i = 0; i < users.length; i++) {
+            if(users[i].age < averageAge){
+                System.out.println("Users younger than avareage are: " + users[i].name);
+            }
+        }
+
+    }
 }
+
+
+
