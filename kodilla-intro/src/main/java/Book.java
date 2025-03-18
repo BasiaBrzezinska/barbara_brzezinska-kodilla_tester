ab24acb7154137eaa85217ed4628dd1189401f3d
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-18 22:07:46 +0100

    Task_2.7

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
 
 public class Book {
+    private String author;
+    private String title;
+git
+    public Book (String title, String author){
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of (String title, String author){
+        return new Book (title, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public static void main (String[] args){
+        Book book = new Book ("1984", " George Orwell");
+        System.out.println("My favorite book is: " + book);
+    }
 }
