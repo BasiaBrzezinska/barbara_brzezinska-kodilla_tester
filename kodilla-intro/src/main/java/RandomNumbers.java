ffde0b71302b017e5a8ef154a198f5893b2063b0
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-20 16:32:17 +0100

    Poprawka_do_uwag

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,14 +1,15 @@
+import javax.swing.plaf.synth.SynthTabbedPaneUI;
 import java.util.Random;
 
 public class RandomNumbers {
+    int maxNumber = Integer.MIN_VALUE;
+    int minNumber = Integer.MAX_VALUE;
 
     public int getCountOfRandomNumbers() {
         int max = 5000;
         Random random = new Random();
         int sum = 0;
         int result = 0;
-        int maxNumber = Integer.MIN_VALUE;
-        int minNumber = Integer.MAX_VALUE;
 
         while (sum < max) {
             int randomNumber = random.nextInt(31);
@@ -22,20 +23,26 @@ public class RandomNumbers {
             if (randomNumber < minNumber) {
                 minNumber = randomNumber;
             }
-        }
 
+        }
 
         System.out.println("Największ wylosowana liczba to: " + maxNumber);
         System.out.println("Najmniejsza wylosowana liczna to: " + minNumber);
         System.out.println("Ilość losowań: " + result);
         return result;
-
     }
 
-    public static void main (String[] args){
+        public int getMaxNumber () {
+        return maxNumber;
+        }
+
+        public int getMinNumber () {
+        return minNumber;
+        }
+
+        public static void main (String[] args){
         RandomNumbers randomNumbers = new RandomNumbers();
         System.out.println(randomNumbers.getCountOfRandomNumbers());
-
-    }
+        }
 }
 
