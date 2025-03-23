62cfc4b08d380551cb057501d9910e3322f1bdaf
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-23 18:00:13 +0100

    Test jednostkowy dla kalkulatora

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuit.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuit.java
@@ -1,4 +1,57 @@
 package com.kodilla.basic_assertion;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTestSuit {
+
+    @Test
+
+    public void testSum(){
+        Calculator calculator = new Calculator();
+        int a = 8;
+        int b = 5;
+        int sumResult = calculator.sum(a, b);
+        assertEquals (13, sumResult );
+
+    }
+
+    @Test
+    public void testSubtract (){
+        Calculator calculator = new Calculator();
+        int a = 8;
+        int b = 5;
+        int subtractResult = calculator.subtract(a,b);
+        assertEquals(3,subtractResult);
+    }
+    @Test
+    public void testPowerPositiveNumber (){
+        Calculator calculator = new Calculator();
+        double base = 2.0;
+        double exponent = 2.0;
+        double powerResult = calculator.power(base,exponent);
+        assertEquals(4.0, powerResult, 0.0001);
+
+    }
+
+    @Test
+    public void testPowerNegativeNumber (){
+        Calculator calculator = new Calculator();
+        double base = -2.0;
+        double exponent = 2.0;
+        double powerResult = calculator.power(base,exponent);
+        assertEquals(4.0, powerResult, 0.0001);
+    }
+
+    @Test
+    public void testPowerZeroNumber (){
+        Calculator calculator = new Calculator();
+        double base = 0.0;
+        double exponent = 2.0;
+        double powerResult = calculator.power(base,exponent);
+        assertEquals(0.0, powerResult, 0.0001);
+    }
+
+
 }
