3b5a9cf679146e7e4ee9fae825656d7bfd94f352
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-03-30 18:14:09 +0200

    task_4.2

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/arrays/homework/CarApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/arrays/homework/CarApplication.java
@@ -1,4 +1,39 @@
 package com.kodilla.collections.interfaces.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Mercedes;
+
+import java.util.Random;
+
 public class CarApplication {
-}
+
+    public static void main (String[] args){
+        Car[] cars = new Car[15];
+        for (int n = 0; n < cars.length; n++)
+           cars [n] = drawCar();
+        for (Car car : cars)
+            CarUtils.describeCar(car);
+    }
+
+    public static Car drawCar (){
+        Random random = new Random();
+        int drawnCarBrand = random.nextInt(3);
+        double a = getRandomSize (random);
+        if (drawnCarBrand == 0)
+            return new Ford(80);
+        else if (drawnCarBrand == 1)
+            return new Kia(90);
+        else {
+            double b = getRandomSize (random);
+            double c = getRandomSize (random);
+            return new Mercedes(120);
+        }
+    }
+
+    private static double getRandomSize(Random random) {
+        return random.nextDouble() * 100 + 1;
+    }
+
+    }
--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/arrays/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/arrays/homework/CarUtils.java
@@ -1,4 +1,27 @@
 package com.kodilla.collections.interfaces.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Mercedes;
+import com.kodilla.collections.interfaces.homework.CarRace;
+
 public class CarUtils {
+    public static void describeCar(Car car) {
+        System.out.println("..............................");
+        System.out.println("Car brand " + getCarName(car));
+        System.out.println("The car is driving at speed " + car.getSpeed());
+    }
+
+    private static String getCarName(Car car) {
+        if (car instanceof Ford)
+            return "Ford";
+        else if (car instanceof Kia)
+            return "Kia";
+        else if (car instanceof Mercedes)
+            return "Mercedes";
+        else
+            return "Unknown brand";
+    }
 }
+
