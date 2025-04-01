c3828dd5eabe0bfe33175a516f7c949b55d45b8f
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-04-01 14:47:50 +0200

    task_4.3

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarListsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarListsApplication.java
@@ -1,4 +1,31 @@
 package com.kodilla.collections.lists.homework;
 
+import com.kodilla.collections.interfaces.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Mercedes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarListsApplication {
+    public static void main(String[] args) {
+
+        List <Car> cars = new ArrayList<>();
+        cars.add(new Ford(120));
+        cars.add(new Mercedes(100));
+        cars.add(new Kia(90));
+
+        Mercedes mercedes = new Mercedes(100);
+
+        cars.remove(0);
+        cars.remove(mercedes);
+
+        for (Car car:cars ){
+            CarUtils.describeCar(car);
+        }
+
+        System.out.println ("Number of objects in the collection: " + cars.size());
+    }
 }
