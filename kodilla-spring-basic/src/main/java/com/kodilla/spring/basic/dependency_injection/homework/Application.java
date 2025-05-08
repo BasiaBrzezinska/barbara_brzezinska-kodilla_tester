23fb5b5d985543d85b8c5e3fdce57b93f1bbd4bb
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-05-08 14:49:51 +0200

    task_9.1_eliminowanie_zależności

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -1,4 +1,13 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
 public class Application {
+
+    public static void main(String[] args) {
+        DeliveryService deliveryService = new Delivery();
+        NotificationService notificationService = new Notification();
+        ShippingCenter shippingCenter = new ShippingCenter(deliveryService, notificationService);
+
+        shippingCenter.sendPackage("Hill street 11, New York", 33);
+        shippingCenter.sendPackage("Wall Street 20, New York", 12.2);
+    }
 }
