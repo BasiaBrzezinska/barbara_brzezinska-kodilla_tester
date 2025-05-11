099eb445538bc0a50ad6596d71ec6f2c51ed4d9e
Basia Brzezinska <basia.brzezinska85@gmail.com>
2025-05-11 13:11:29 +0200

    task_9.3

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
@@ -1,4 +1,26 @@
 package com.kodilla.spring.basic.spring_dependency_injection.homework;
 
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ShippingCenter {
+
+
+    @Resource (name = "notification")
+    private NotificationService notificationService;
+
+    @Resource (name = "delivery")
+    private DeliveryService deliveryService;
+
+
+    public String sendPackage(String address, double weight) {
+        if (deliveryService.deliverPackage(address, weight)) {
+            return notificationService.success(address);
+        }
+        return notificationService.fail(address);
+    }
 }
+
